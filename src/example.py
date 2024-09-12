72f71ae1c3ec6fc7c435ce20707610cabf422154
Martin Perez <martinperezrodriguez@gmail.com>
2024-09-12 15:10:41 -0400

    added new files logs

--- src/example.py
+++ src/example.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 pd.set_option('display.max_columns', None)
+pd.set_option("display.max_rows", None)
 
 
 
@@ -18,6 +19,8 @@ def fn_data_analysis(data_df, logger):
     logger.info(f"Dataframe tail: {data_df.tail()}")
     logger.info(f"Dataframe null values: {data_df.isnull().sum()}")
     logger.info(f"Dataframe duplicated values: {data_df.duplicated().sum()}")
+
+    print(data_df.shape)
 
 def fn_fetch_data(logger):
     csv_file_path = '../local_data/titanic.csv'
